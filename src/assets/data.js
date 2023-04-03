508548483afdd8d3a852b6be9d86dab2c4a014b2
Buvaneshwari <thirubuvana@gmail.com>
2023-04-03 16:08:46 +0530

    projects added

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,45 +1,52 @@
-import Img1 from './ecom.webp'
+
+import mealDash from '../assets/mealDash.png'
+import spiceMart from '../assets/spiceMart.png'
+import youTube from '../assets/youTubeStatic.png'
+import portFolio from '../assets/portfolio.png'
+import ticTacToe from '../assets/ticTacToe.png'
+import weatherApp from '../assets/weather.png'
 
 export const data=[{
     id:1,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:mealDash,
+    title:"MealDash-Order your meal",
+    desc:"App build by fetching data of zomato API - React JS",
+    type:"frontend",
+    github:'https://github.com/BuviThiru/MealDash',
+    demo:"https://buvi-meal-dash.netlify.app/",
 },{
     id:2,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:spiceMart,
+    title:"SpiceMart- check our spices",
+    desc:"A sample static page of a spice shop which shows the list of spices sold",
+    github:'https://github.com/BuviThiru/SpiceMart',
+    demo:"https://buvi-spice-mart.netlify.app/",
 },{
     id:3,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:youTube,
+    title:"My YouTube",
+    desc: "YouTube Clone - with HTML,CSS and Javascript",
+    github:'https://github.com/BuviThiru/YouTube-Clone-Front-End-',
+    demo:"https://youtube-static-buvi.netlify.app/",
 },{
     id:4,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:portFolio,
+    title:"This my portfolio",
+    desc:"This is my portfolio build using React JS",
+    github:'https://github.com/BuviThiru/Portfolio',
+    demo:"https://buvaneshwari-portfolio-resume.netlify.app/#header",
 },{
     id:5,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:ticTacToe,
+    title:"Tic-Tac-Toe",
+    desc:"A game to chill",
+    github:'https://github.com/BuviThiru/Tic-Tac-Toe',
+    demo:"https://tic-tac-toe-buvi-fun.netlify.app/",
 },{
     id:6,
-    image:Img1,
-    title:"This is portfolio",
-    desc:"This is a small description about the project I build",
-    github:'',
-    demo:"",
+    image:weatherApp,
+    title:"Weather App",
+    desc:"Find the weather in your city",
+    github:'https://github.com/BuviThiru/weather-app',
+    demo:"https://buvi-weatherapp.netlify.app/",
 }]
